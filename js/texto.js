ff3b0f8626efb93e73dfd0c158b63880b25307ed
AELD73 <167131119+AELD73@users.noreply.github.com>
2024-05-28 12:57:47 -0600

    Update texto.js

--- js/texto.js
+++ js/texto.js
@@ -1,39 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const thumbnailsContainer = document.querySelector('.slider-thumbnails');
-  const thumbnails = document.querySelectorAll('.thumbnail');
-  
-    const images = document.querySelectorAll('.slider-images img');
-    const overlay = document.querySelector('.overlay');
-    const overlayImage = overlay.querySelector('img');
-  const thumbnailWidth = thumbnails[0].offsetWidth;
-  const thumbnailsCount = thumbnails.length;
-  let currentIndex = 0;
+  const images = document.querySelectorAll('.slider-images img');
+  const overlay = document.querySelector('.overlay');
+  const overlayImage = overlay.querySelector('img');
 
-  // Función para desplazar las miniaturas automáticamente
-  function moveThumbnails() {
-    currentIndex = (currentIndex + 1) % thumbnailsCount;
-    const offset =-(currentIndex * thumbnailWidth);
-    thumbnailsContainer.style.transform = `translateX(${offset}px)`;
-    setTimeout(moveThumbnails, 5000); // Cambia la miniatura cada 2 segundos
-  }
-
-  moveThumbnails(); // Iniciar el movimiento automático de las miniaturas
-
-  //Código correspondiente para hacer click sobre la 
-  thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-      currentIndex = index;
-    });
-  });
-  thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-      overlayImage.src = images[index].src;
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      overlayImage.src = image.src;
       overlay.style.display = 'flex';
     });
   });
+
   overlay.addEventListener('click', () => {
     overlay.style.display = 'none';
   });
-
-  
 });
