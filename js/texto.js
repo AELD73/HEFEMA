d9f037dafaaf30982d75de033ae732ec0be6e110
AELD73 <167131119+AELD73@users.noreply.github.com>
2024-06-05 19:09:15 -0600

    Update texto.js

--- js/texto.js
+++ js/texto.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const images = document.querySelectorAll('.slider-images img');
   const overlay = document.querySelector('.overlay');
   const overlayImage = overlay.querySelector('img');
+  const overlayText = overlay.querySelector('.overlay-text');
 
   images.forEach(image => {
     image.addEventListener('click', () => {
       overlayImage.src = image.src;
+      overlayText.textContent = image.getAttribute('data-text');
       overlay.style.display = 'flex';
     });
   });
