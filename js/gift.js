e7d931228980fbdbbd4e3b8b1ac1d56b878c1bbf
AELD73 <167131119+AELD73@users.noreply.github.com>
2024-07-02 01:07:23 -0600

    Update gift.js

--- js/gift.js
+++ js/gift.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gif = document.getElementById('inicio');
-    gif.src = 'imagenes/HERRAJES FERROVIARIOS - BIENVENIDA 6.gif';
+    gif.src = 'imagenes/HERRAJES FERROVIARIOS - BIENVENIDA2.gif';
     gif.onload = () => {
         gif.style.display = 'block'; // Muestra la imagen después de cargar
     };
